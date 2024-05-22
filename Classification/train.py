29583007cd1075f98ab4d11f4ccfd056f81220e6
Trung Tran <tritrungtq@gmail.com>
2024-05-22 14:01:58 +0700

    fix(file): add file

--- Classification/train.py
+++ Classification/train.py
@@ -1,11 +1,12 @@
 import pandas as pd
-import Classification.classification_utility as classification_utility
+import classification_utility
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import joblib
 
 def Train():
-    df = pd.read_csv("../data/outlier_included.csv")
+    df = pd.read_csv("../data/update_dataset.csv")
 
     label_to_num = {
         'Very weak': 1,
@@ -19,6 +20,8 @@ def Train():
     X = df.drop(["password", "strength", "class_strength"], axis=1)
     y = df["class_strength"]
 
+    print(y.head())
+
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -30,13 +33,15 @@ def Train():
         'metric': 'euclidean'
     }
 
+    print(f"sklearn version: {sklearn.__version__}")
+    print(f"joblib version: {joblib.__version__}")
     model = KNeighborsClassifier(**hyperparameters)
     model.fit(X, y)
 
     print(f"Hyperparameters: {hyperparameters}")
     
     classification_utility.evaluate(model, X_train, y_train, X_test, y_test)
-    joblib.dump(model, '../model/final/knn_classification.pkl')
+    joblib.dump(model, '../model/retrain_knn_classification.pkl')
     print("TRAINING FINISHED")
 
 if __name__ == "__main__":
